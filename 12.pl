e3a2fe2a223dec6afba21d64d840629413d1c493
Igor Korolev <korolev-ia@yandex.ru>
2016-07-13 17:37:13 +0300

    20160713-02
    -

--- 12.pl
+++
@@ -1,6 +0,0 @@
-@a=qw( lane sample_id sample_name sample_plate sample_well i7_index_id index0 sample_project description run_id );
-$i=0;
-foreach( @a ) {
-	print "\$hrow{$_}=substr( \$row[$i], 0, 45 ) ;\n" ;
-	$i++;
-}
